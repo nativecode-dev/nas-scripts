b416f8a895cfc2acd55c31cd7949263f500ebead
= <=>
2015-10-23 23:16:09 -0500

    Changing directory structure

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cd /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts
 
-ln -s /share/Data/Nzbget-Scripts/Completion.py Completion.py
-ln -s /share/Data/Nzbget-Scripts/EventHelper.py EventHelper.py
-ln -s /share/Data/Nzbget-Scripts/FileMover.py FileMover.py
-ln -s /share/Data/Nzbget-Scripts/Rejector.py Rejector.py
-ln -s /share/Data/Nzbget-Scripts/nzb.py nzb.py
-ln -s /share/Data/Nzbget-Scripts/nzbutils.py nzbutils.py
+ln -s /share/Data/Scripts/nzbget/Completion.py Completion.py
+ln -s /share/Data/Scripts/nzbget/EventHelper.py EventHelper.py
+ln -s /share/Data/Scripts/nzbget/FileMover.py FileMover.py
+ln -s /share/Data/Scripts/nzbget/Rejector.py Rejector.py
+ln -s /share/Data/Scripts/nzbget/nzb.py nzb.py
+ln -s /share/Data/Scripts/nzbget/nzbutils.py nzbutils.py
