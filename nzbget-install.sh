604d94ce4988beb31fb544c802ca7e998d4319c8
Mike Pham <mpham@nativecode.com>
2015-10-27 23:06:43 -0500

    Changed NZBGET install script

--- nzbget-install.sh
+++ nzbget-install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-echo '$0 = ' $0
-echo '$1 = ' $1
+ln -s /share/Data/Source/nas-scripts/nzbget/EventHelper.py \
+    /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/EventHelper.py
 
-cd /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts
+ln -s /share/Data/Source/nas-scripts/nzbget/FileMover.py \
+    /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/FileMover.py
 
-ln -s /share/Data/Scripts/nzbget/Completion.py Completion.py
-ln -s /share/Data/Scripts/nzbget/EventHelper.py EventHelper.py
-ln -s /share/Data/Scripts/nzbget/FileMover.py FileMover.py
-ln -s /share/Data/Scripts/nzbget/Rejector.py Rejector.py
-ln -s /share/Data/Scripts/nzbget/nzb.py nzb.py
+ln -s /share/Data/Source/nas-scripts/nzbget/Rejector.py \
+    /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/Rejector.py
+
+ln -s /share/Data/Source/nas-scripts/nzbget/nzb.py \
+    /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/nzb.py
