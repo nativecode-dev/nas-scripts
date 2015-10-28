8189e59f996bba08295c66d1100b394704c51214
Mike Pham <mpham@nativecode.com>
2015-10-27 22:52:32 -0500

    Added logging to deluge script

--- deluge/execute_scripts/completed.py
+++ deluge/execute_scripts/completed.py
@@ -5,20 +5,56 @@ import logging.handlers
 import os
 import sys
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-logger.addHandler(logging.handlers.SysLogHandler(address=('localhost', 514)))
+
+def _setup_logging():
+    logger = logging.getLogger('Deluge')
+    logger.setLevel(logging.DEBUG)
+
+    _setup_logging_handlers(logger)
+
+    return logger
+
+
+def _setup_logging_handlers(logger):
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Console
+    handler_console = logging.StreamHandler(stream=sys.stdout)
+    handler_console.setLevel(logging.DEBUG)
+    logger.addHandler(handler_console)
+
+    # SysLog
+    handler_syslog = logging.handlers.SysLogHandler(address='/dev/log')
+    handler_syslog.setFormatter(formatter)
+    handler_syslog.setLevel(logging.DEBUG)
+    logger.addHandler(handler_syslog)
+
+    # File
+    logfile = '/share/Data/Logs/Deluge/completed.log'
+    handler_file = logging.handlers.TimedRotatingFileHandler(logfile, when='midnight')
+    handler_file.setFormatter(formatter)
+    handler_file.setLevel(logging.INFO)
+    logger.addHandler(handler_file)
+
+
+def _torrent_completed(logger, id, name, directory):
+    logger.info('torrent_completed(%s, %s, %s)' % (id, name, directory))
+
+    if os.path.isdir(directory):
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                path = os.path.join(root, file)
+                logger.debug(path)
+
 
 def main():
-    logger.info('Deluge called completed.')
+    logger = _setup_logging()
 
     torrent_id = sys.argv[1]
     torrent_name = sys.argv[2]
     torrent_dir = sys.argv[3]
 
-    # Log the parameters that were passed.
-    logger.info('Torrent hash: %s.' % torrent_id)
-    logger.info('Torrent name: %s.' % torrent_name)
-    logger.info('Torrent dir: %s.' % torrent_dir)
+    _torrent_completed(logger, torrent_id, torrent_name, torrent_dir)
+
 
 main()
