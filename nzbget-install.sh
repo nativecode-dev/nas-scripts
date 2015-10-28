2214e86c41f3d0226f761809052c73a1c3b8f1a5
Mike Pham <mpham@nativecode.com>
2015-10-28 01:07:43 -0500

    Added HealthCheck to install script

--- nzbget-install.sh
+++ nzbget-install.sh
@@ -3,6 +3,9 @@
 ln -s /share/Data/Source/nas-scripts/nzbget/EventHelper.py \
     /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/EventHelper.py
 
+ln -s /share/Data/Source/nas-scripts/nzbget/HealthCheck.py \
+    /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/HealthCheck.py
+
 ln -s /share/Data/Source/nas-scripts/nzbget/FileMover.py \
     /volume1/.@plugins/AppCentral/nzbget/nzbget/scripts/FileMover.py
 
