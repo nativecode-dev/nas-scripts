6a5ec589f9aed5fd488f0d6d581d0288aa6dce63
Mike Pham <mpham@nativecode.com>
2015-11-15 04:10:24 -0600

    Added logging messages

--- pyscripts/monitor.py
+++ pyscripts/monitor.py
@@ -3,6 +3,7 @@
 # IMPORTS
 # -----------------------------------------------------------------------------
 import argparse
+import json
 import logging
 import logging.handlers
 import sys
@@ -35,7 +36,7 @@ def main():
 
 def initialize_loggers():
     global log
-    log = logging.Logger('sitemonitor')
+    log = logging.Logger('monitor')
     create_console_logger()
     create_syslog_logger()
 
@@ -48,18 +49,22 @@ def create_console_logger():
 
 def create_syslog_logger():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    pass
+    syslog = logging.handlers.SysLogHandler(address='/dev/log')
+    syslog.setLevel(logging.ERROR)
+    syslog.setFormatter(formatter)
+    log.addHandler(syslog)
 
 
 def initialize_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--command')
-    parser.add_argument('--debug', default=False, type=bool)
-    parser.add_argument('--verbose', default=False, type=bool)
     subparsers = parser.add_subparsers()
 
     # Define subcommand for checking configured sites.
     initialize_arguments_check(subparsers)
+
+    # Define subcommand for showing configuration options.
+    initialize_arguments_config(subparsers)
 
     # Define subcommand for checking connections.
     initialize_arguments_connection(subparsers)
@@ -84,6 +89,11 @@ def initialize_arguments_check(subparsers):
     parser = subparsers.add_parser('check', help='Perform checks on configured targets.')
     parser.add_argument('--monitor', choices=['all', 'connections', 'sites'], default='all', type=str)
     parser.set_defaults(func=perform_checks)
+
+
+def initialize_arguments_config(subparsers):
+    parser = subparsers.add_parser('config', help='Show configuration options.')
+    parser.set_defaults(func=show_config)
 
 
 def initialize_arguments_connection(subparsers):
@@ -145,6 +155,7 @@ def perform_check_sites(args, config):
     sites = config['sites']
 
     for url in sites:
+        log.info("Checking site '%s'." % url)
         site = sites[url]
 
         try:
@@ -154,10 +165,11 @@ def perform_check_sites(args, config):
                 headers = http.get_basic_auth_header(site['username'], site['password'])
                 response = http.get(url, headers)
 
-            if response and response.code == 200 and args.verbose:
+            if response and response.code == 200:
                 log.info("Site %s returned a 200 OK status." % url)
         except urllib2.HTTPError as e:
             message = "Site %s returned a '%s (%s)' status." % (url, e.reason, e.code)
+            log.debug(message)
             send_notifications(config, message, url)
 
 
@@ -166,6 +178,7 @@ def send_notifications(config, message, url):
 
     if notifiers:
         for type in notifiers.keys():
+            log.info("Sending notification via %s." % type)
             try:
                 if type == 'email':
                     email.send_multiple(notifiers[type], message)
@@ -187,15 +200,19 @@ def modify_notification(args, config):
         notifier = notifiers[args.type] = {} if not args.type in notifiers else notifiers[args.type]
         if args.type == 'email':
             conf.list_add(notifier, 'recipients', conf.list_split(args.options))
+            log.info("Added email recipients %s." % args.options)
         elif args.type == 'pushover':
             conf.dict_addstring(notifier, args.options, ['apikey', 'clientkey'])
+            log.info("Added pushover options %s." % args.options)
     elif args.action == 'remove':
         if args.type in notifiers:
             notifier = notifiers[args.type]
             if args.type == 'email':
                 conf.list_remove(notifier, 'recipients', conf.list_split(args.options))
+                log.info("Removed email recipients %s." % args.options)
             elif args.type == 'pushover':
                 conf.dict_remove(notifier, args.options)
+                log.info("Removed pushover channel %s." % args.options)
 
     conf.write_json(CONFIG_NAME, config)
 
@@ -216,6 +233,10 @@ def modify_site(args, config):
             log.info("Removed site %s." % args.url)
 
     conf.write_json(CONFIG_NAME, config)
+
+
+def show_config(args, config):
+    print(json.dumps(config, sort_keys=True, indent=2, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
